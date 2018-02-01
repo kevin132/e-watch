ad3301beb014b45a9f39b8b44a51542c09d749b8
kevin132 <chenkevin@hotmail.fr>
2018-02-01 23:38:02 +0100

    project

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport"
+           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+     <title>E-watch</title>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/4AjdAObqC74" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+     <ul class="navbar-nav">
+          <li class="nav-item active">
+               <a class="nav-link" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+     </ul>
+</nav>
+<header>
+     <div class="row">
+          <div class="col-lg-5"></div>
+          <div class="col-lg-7 text-center">
+               <img src="watch.jpg" class="head-watch" alt="watch">
+          </div>
+     </div>
+</header>
+
+<section class="block-two">
+     <div class="container">
+          <div class="row">
+               <div class="col-lg-3 text-center">
+                    <div class="block-circle"></div>
+               </div>
+               <div class="col-lg-3 text-center">
+                    <div class="block-circle"></div>
+               </div>
+               <div class="col-lg-3 text-center">
+                    <div class="block-circle"></div>
+               </div>
+               <div class="col-lg-3 text-center">
+                    <div class="block-circle"></div>
+               </div>
+          </div>
+     </div>
+</section>
+
+
+</body>
+</html>

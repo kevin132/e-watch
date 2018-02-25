5f18139c7e9a8e1889f6997b975e7507b600e4d8
kevin132 <chenkevin@hotmail.fr>
2018-02-26 00:47:34 +0100

    add blog

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,12 +2,12 @@
 
 $(document).ready(function(){
     $(".button a").click(function(){
-        $(".overlay").fadeToggle(200);
+        $(".overlay").fadeToggle(100);
         $(this).toggleClass('btn-open').toggleClass('btn-close');
     });
 });
 $('.overlay').on('click', function(){
-    $(".overlay").fadeToggle(200);
+    $(".overlay").fadeToggle(100);
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
 });
